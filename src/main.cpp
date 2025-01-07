b95fa0c2dabadf6b7f9dc41ee63477de7bdeedd4
Metafysical <wulff43211234@gmail.com>
2025-01-07 13:57:29 +0100

    Added comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,6 @@
 #define YELLOWPIN 11
 #define GREENPIN 12
 
-// put function declarations here:
-
 void setup() {
   Serial.begin(9600); //Begins serial communication
   
@@ -13,18 +11,27 @@ void setup() {
   pinMode(YELLOWPIN, OUTPUT); 
   pinMode(GREENPIN, OUTPUT); 
 
-  //Turns the red light on, and the others off
+  //ensures the lights are off
   digitalWrite(REDPIN, LOW);
   digitalWrite(YELLOWPIN, LOW);
   digitalWrite(GREENPIN, LOW);
 }
 
 void loop() {
+  //Three bits give a total of 2^3 = 8 different combinations. As such we run a for loop for eight times
   for (int i = 0; i < 8; i++) {
-    digitalWrite(GREENPIN, (i % 2));
-    digitalWrite(YELLOWPIN, ((i / 2) % 2)); 
-    digitalWrite(REDPIN, ((i / 4) % 2)); 
-    Serial.println(String(((i / 4) % 2)) + String(((i / 2) % 2)) + String((i % 2)));
-    delay(500);     
+    int LSB = (i % 2); //For the least significant bit, we run a modulus with two, which equals on, whenever 'i' is uneven
+    int MB = ((i / 2) % 2); //For the middle bit, we first divide 'i' with two, which gets rounded down, before using modulus. This makes the value switch between 1 and 0 for every other number
+    int MSB = ((i / 4) % 2); //For the most significant bit we do the same as with the middle bit, but divide with 4 instead, making it change only every four times. 
+    //Writes the bit values to the LEDs
+    digitalWrite(GREENPIN, LSB); 
+    digitalWrite(YELLOWPIN, MB); 
+    digitalWrite(REDPIN, MSB); 
+    //Prints out the current bits
+    Serial.println(String(MSB) + String(MB) + String(LSB)); //Converts the individual bits to strings, in order to combine them
+    delay(500); //A 500ms pause between counting one up
   }
 }
+
+//2d: 
+//We did use the % operator
