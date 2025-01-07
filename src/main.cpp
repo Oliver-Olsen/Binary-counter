76ae50936a51d009f3455a7fd90b1fc9144669dc
Metafysical <wulff43211234@gmail.com>
2025-01-07 13:10:54 +0100

    Working code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
+#define REDPIN 10
+#define YELLOWPIN 11
+#define GREENPIN 12
 
 // put function declarations here:
-int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600); //Begins serial communication
+  
+  //Defines pinmodes
+  pinMode(REDPIN, OUTPUT); 
+  pinMode(YELLOWPIN, OUTPUT); 
+  pinMode(GREENPIN, OUTPUT); 
+
+  //Turns the red light on, and the others off
+  digitalWrite(REDPIN, LOW);
+  digitalWrite(YELLOWPIN, LOW);
+  digitalWrite(GREENPIN, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(GREENPIN, (i % 2));
+    digitalWrite(YELLOWPIN, ((i / 2) % 2)); 
+    digitalWrite(REDPIN, ((i / 4) % 2)); 
+    Serial.println(String(((i / 4) % 2)) + String(((i / 2) % 2)) + String((i % 2)));
+    delay(500);     
+  }
 }
